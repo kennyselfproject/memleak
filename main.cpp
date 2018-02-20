69871fa4a38c1f932c989b25e953d9eb6c8b04cc
wxzkenny <wxzkenny@aliyun.com>
2018-02-20 06:38:35 -0800

    add test case for memleak check

--- main.cpp
+++ main.cpp
@@ -31,9 +31,9 @@ void test_delete_first()
 
 void test_delete_median()
 {
-    int * ptr1 = new int(2);
-    int * ptr2 = new int[10];
-    test * ptr3 = new test(0);
+    int * ptr1 = new (NULL) int(2);
+    int * ptr2 = new (NULL) int[10];
+    test * ptr3 = new (NULL) test(0);
     /*
       delete ptr; 调用的就是 void operator delete(void * ptr); 而与new 匹配的delete 不是自己调用的
       ，而是在new申请,成功却在构造函数时候出错，new operator自己根据operator new 来寻找 
@@ -49,9 +49,9 @@ void test_delete_median()
 
 void test_delete_end()
 {
-    int * ptr1 = new int(2);
-    int * ptr2 = new int[10];
-    test * ptr3 = new test(0);
+    int * ptr1 = new (NULL) int(2);
+    int * ptr2 = new (NULL) int[10];
+    test * ptr3 = new (NULL) test(0);
     /*
       delete ptr; 调用的就是 void operator delete(void * ptr); 而与new 匹配的delete 不是自己调用的
       ，而是在new申请,成功却在构造函数时候出错，new operator自己根据operator new 来寻找 
@@ -67,9 +67,9 @@ void test_delete_end()
 
 void test_delete_all()
 {
-    int * ptr1 = new int(2);
-    int * ptr2 = new int[10];
-    test * ptr3 = new test(0);
+    int * ptr1 = new (NULL) int(2);
+    int * ptr2 = new (NULL) int[10];
+    test * ptr3 = new (NULL) test(0);
     /*
       delete ptr; 调用的就是 void operator delete(void * ptr); 而与new 匹配的delete 不是自己调用的
       ，而是在new申请,成功却在构造函数时候出错，new operator自己根据operator new 来寻找 
@@ -87,9 +87,9 @@ void test_delete_all()
 int main()
 {
     test_delete_first();
-// test_delete_median();
-//     test_delete_end();
-//     test_delete_all();
+    test_delete_median();
+    test_delete_end();
+    test_delete_all();
 
     return 0;
 }
